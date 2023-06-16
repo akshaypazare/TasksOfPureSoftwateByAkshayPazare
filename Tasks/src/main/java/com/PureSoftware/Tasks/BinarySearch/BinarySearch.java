d2416b4d691a36221578ffaa26de1c2fa593fdce
akshaypazare <126969424+akshaypazare@users.noreply.github.com>
2023-06-16 12:33:37 +0530

    Update BinarySearch.java

--- Tasks/src/main/java/com/PureSoftware/Tasks/BinarySearch/BinarySearch.java
+++ Tasks/src/main/java/com/PureSoftware/Tasks/BinarySearch/BinarySearch.java
@@ -6,23 +6,20 @@ public class BinarySearch {
         if (right >= left) {
             int mid = left + (right - left) / 2;
 
-            // If the target is found, return its index
+
             if (arr[mid] == target) {
                 return mid;
             }
 
-            // If the target is smaller than the middle element, search in the left subarray
             else if (arr[mid] > target) {
                 return binarySearch(arr, target, left, mid - 1);
             }
 
-            // If the target is larger than the middle element, search in the right subarray
             else {
                 return binarySearch(arr, target, mid + 1, right);
             }
         }
 
-        // If the remaining array to be searched is empty, return -1
         else {
             return -1;
         }
