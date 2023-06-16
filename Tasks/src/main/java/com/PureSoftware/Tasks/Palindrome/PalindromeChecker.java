ffcf2b99ca71dbcdbff9c285ff80a13f539a09ca
akshaypazare <126969424+akshaypazare@users.noreply.github.com>
2023-06-16 12:32:01 +0530

    Update PalindromeChecker.java

--- Tasks/src/main/java/com/PureSoftware/Tasks/Palindrome/PalindromeChecker.java
+++ Tasks/src/main/java/com/PureSoftware/Tasks/Palindrome/PalindromeChecker.java
@@ -2,7 +2,7 @@ package com.PureSoftware.Tasks.Palindrome;
 
 public class PalindromeChecker {
 
-    public static String checkPalindrome(String string) {
+    public static boolean checkPalindrome(String string) {
 
         String cleanString = string.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
@@ -10,12 +10,21 @@ public class PalindromeChecker {
         for (int i = 0; i < length / 2; i++) {
             if (cleanString.charAt(i) != cleanString.charAt(length - 1 - i)) {
 
-                return string + " is not palindrome.";
+                return false;
             }
         }
 
-        return string + " is a palindrome.";
+        return true;
     }
 
+    public static void main(String[] args) {
+        String word = "radar3";
+        if (checkPalindrome(word)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
 
+
+    }
 }
